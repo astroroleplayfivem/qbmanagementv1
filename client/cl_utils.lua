8ce3687fbebebb7afc85292c0d0ff27daf2d526c
Astro Scripts I A Division of Astro RP <vynterstwitch@gmail.com>
2026-04-29 01:40:18 -0400

    Create cl_utils.lua

---
+++ client/cl_utils.lua
@@ -0,0 +1,18 @@
+local function deepCopy(orig)
+    local orig_type = type(orig)
+    local copy
+
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepCopy(orig_key)] = deepCopy(orig_value)
+        end
+        setmetatable(copy, deepCopy(getmetatable(orig)))
+    else
+        copy = orig
+    end
+
+    return copy
+end
+
+exports('deepCopy', deepCopy)
