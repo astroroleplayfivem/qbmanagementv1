2d6fdb8a4dcbb33fa6a916288826bdb75b8b5606
Astro Scripts I A Division of Astro RP <vynterstwitch@gmail.com>
2026-04-29 01:46:25 -0400

    Create sh_businesses.lua

---
+++ shared/sh_businesses.lua
@@ -0,0 +1,97 @@
+Businesses = Businesses or {}
+
+Businesses.whitewidow = {
+    job = 'whitewidow',
+    label = 'White Widow',
+    subtitle = 'Weed Store',
+    theme = 'dispensary',
+    employeeLabel = 'Staff'
+}
+
+Businesses.catcafe = {
+    job = 'catcafe',
+    label = 'Cat Cafe',
+    subtitle = 'UWU Cafe',
+    theme = 'cafe',
+    employeeLabel = 'Team Members'
+}
+
+Businesses.rideout = {
+    job = 'rideout',
+    label = 'Rideout',
+    subtitle = 'Mechanic Shop',
+    theme = 'mechanic',
+    employeeLabel = 'Technicians'
+}
+
+Businesses.harmony = {
+    job = 'harmony',
+    label = 'Harmony',
+    subtitle = 'Mechanic Shop',
+    theme = 'mechanic',
+    employeeLabel = 'Technicians'
+}
+
+Businesses.hayes = {
+    job = 'hayes',
+    label = 'Hayes',
+    subtitle = 'Mechanic Shop',
+    theme = 'mechanic',
+    employeeLabel = 'Technicians'
+}
+
+Businesses.ambulance = {
+    job = 'ambulance',
+    label = 'EMS',
+    subtitle = 'Emergency Medical Services',
+    theme = 'emergency_ems',
+    employeeLabel = 'Medics'
+}
+
+Businesses.police = {
+    job = 'police',
+    label = 'Police Department',
+    subtitle = 'Law Enforcement',
+    theme = 'emergency_police',
+    employeeLabel = 'Officers'
+}
+
+Businesses.wattersonstowing = {
+    job = 'wattersonstowing',
+    label = 'Wattersons Towing',
+    subtitle = 'Towing Department',
+    theme = 'towing',
+    employeeLabel = 'Operators'
+}
+
+Businesses.tequilala = {
+    job = 'tequilala',
+    label = 'Tequi-la-la',
+    subtitle = 'Nightclub',
+    theme = 'tequilala',
+    employeeLabel = 'Staff'
+}
+
+Businesses.tequilala = {
+    job = 'tequilala',
+    label = 'Tequi-la-la',
+    subtitle = 'Nightclub',
+    theme = 'tequilala',
+    employeeLabel = 'Staff'
+}
+
+Businesses.vanilla = {
+    job = 'vanilla',
+    label = 'Vanilla Unicorn',
+    subtitle = 'Nightclub',
+    theme = 'tequilala',
+    employeeLabel = 'Staff'
+}
+
+Businesses.burgershot = {
+    job = 'burgershot',
+    label = 'Burger Shot',
+    subtitle = 'Fast Food Restaurant',
+    theme = 'burgershot',
+    employeeLabel = 'Crew Members'
+}
